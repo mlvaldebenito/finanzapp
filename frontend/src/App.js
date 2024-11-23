1d5433772a558bb32493b71372dcc77cad843eaa
felipe-barria95 <frbarria@uc.cl>
2024-11-23 13:02:19 -0300

    feat(): add login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MyNewView from './views/MyNewView';
 import MainView from './views/MainView';
 import OnBoardingView from './views/OnBoardingView';
+import Login from './views/Login';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MyNewView />} />
         <Route path="/main" element={<MainView />} />
+        <Route path="/login" element={<Login />} />
         {/* Add more routes here */}
         <Route path="/onboarding" element={<OnBoardingView />} />
       </Routes>
