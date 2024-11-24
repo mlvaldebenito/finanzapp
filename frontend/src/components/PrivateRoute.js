f6412920869ec8983239472288a0c98abef421be
felipe-barria95 <frbarria@uc.cl>
2024-11-24 00:39:22 -0300

    feat()

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import useIsLogged from  '../hooks/useIsLogged'; // Import the useIsLogged hook
+import useGetUser from '../hooks/useGetUser';
 
 const PrivateRoute = ({ children }) => {
+  const user = useGetUser();
   const isLogged = useIsLogged(); // Check if the user is logged in
-
+  if (user && !user.hasBankCredentials) return <Navigate to="/register-credentials" replace />;
   return isLogged ? children : <Navigate to="/login" replace />;
 };
 
